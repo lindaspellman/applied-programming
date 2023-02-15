4f9da6903140fd0995488473fffe341d2de5b856
lindaspellman <lindaspellman@live.com>
2023-02-14 19:44:41 -0700

    created book table in library db

--- sqlite_relational_database/README.md
+++ sqlite_relational_database/README.md
@@ -1,6 +1,15 @@
 # Overview
 
-Currently, I don't have any work to show for this project as of starting it about two weeks ago, so I'm going to continue this project for the next two weeks. No need to read further right now.
+For the README.md:
+{Important! Do not say in this section that this is college assignment. Talk about what you are trying to accomplish as a software engineer to further your learning.}
+
+{Provide a description of the software that you wrote and how it integrates with a SQL Relational Database. Describe how to use your program.}
+
+{Describe your purpose for writing this software.}
+
+{Provide a link to your YouTube demonstration. It should be a 4-5 minute demo of the software running, a walkthrough of the code, and a view of how created the Relational Database.}
+
+[Software Demo Video](http://youtube.link.goes.here)
 
 
 # Relational Database
--- sqlite_relational_database/learning_notes.py
+++ sqlite_relational_database/learning_notes.py
@@ -1,17 +1,69 @@
+# %%
 # Review of SQLite and Learning More About It
-import pandas as p
+# import pandas and sqlite3 modules
+import pandas as pd
 import sqlite3 as s 
-import plotnine as pn
+# import plotnine as pn
 
+# The purpose of this project is to work with a "book" table of the "library" database
+
+# library TABLE: library_id, library_name, 
+# A library can have many books and many borrowers
+# borrower TABLE: borrower_id, first_name, last_name
+# A borrower can have many books 
+# book TABLE: book_id, author_first, author_last, summary, page_number, borrower_last_issued, issue_duration
+# %%
+# connect to library database
+con = s.connect('c:\sqlite\db\library.db')
+
+# %%
+# create book table
+df = pd.read_sql_query("""
+
+CREATE TABLE book
+    ( book_id, author_first, author_last, summary, page_number, borrower_last_issued, issue_duration)
+
+""", con)
+
+# %%
+# insert data into book table 
+
+
+# %%
 """
-For the README.md:
-{Important! Do not say in this section that this is college assignment. Talk about what you are trying to accomplish as a software engineer to further your learning.}
+# To access the sqlite command line tool, navigate to your c:\sqlite folder with 
+# "cd c:\sqlite" 
+# and type in 
+# "sqlite3" 
+# This should bring up the sqlite prompt. 
 
-{Provide a description of the software that you wrote and how it integrates with a SQL Relational Database. Describe how to use your program.}
+To open a specific database
+sqlite> .open c:\sqlite\db\chinook.db
 
-{Describe your purpose for writing this software.}
+# to open a specific database at the same time you are entering the sqlite command-line terminal
+>sqlite3 c:\sqlite\db\chinook.db
+SQLite version 3.13.0 2016-05-18 10:57:30
+Enter ".help" for usage hints.
+sqlite>
 
-{Provide a link to your YouTube demonstration. It should be a 4-5 minute demo of the software running, a walkthrough of the code, and a view of how created the Relational Database.}
+# If you start a session with a database name that does not exist, the sqlite3 tool will create the database file.
+# for example
+>sqlite3 c:\sqlite\db\sales.db
+SQLite version 3.29.0 2019-07-10 17:32:03
+Enter ".help" for usage hints.
+sqlite>
+# sales.db did not exist but now does
 
-[Software Demo Video](http://youtube.link.goes.here)
+# To show all available commands and their purpose
+.help
+
+# To show all databases in the current connection
+.databases
+
+# To add an additional database in the current connection
+sqlite> ATTACH DATABASE "c:\sqlite\db\chinook.db" AS chinook;
+
+# To exit the sqlite3 program
+.exit 
 """
+# %%
